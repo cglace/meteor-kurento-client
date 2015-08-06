b4abb6cbefd43745c6e9bca3578672adb5933883
cglace <cglace@silverpop.com>
2015-08-06 14:40:14 -0700

    bump version again

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'Media API for the Kurento Web SDK via the NPM kurento-client-js package',
-    version: "0.0.3",
+    version: "0.0.4",
     name: "cglace:kurento-client",
     git: "https://github.com/cglace/meteor-kurento-client"
 });
