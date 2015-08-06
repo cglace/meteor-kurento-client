dbb61c5c4c7ad3b40190d16f3c8ae228ddf5cd55
cglace <cglace@silverpop.com>
2015-08-06 11:28:49 -0700

    Add Readme update package.js

---
+++ README.md
@@ -0,0 +1,4 @@
+## Wrapper for the [kurento-client](https://www.npmjs.com/package/kurento-client) npm package
+
+JavaScript Kurento Client for web applications and Node.js.
+
--- package.js
+++ package.js
@@ -1,14 +1,18 @@
 Package.describe({
     summary: 'Media API for the Kurento Web SDK via the NPM kurento-client-js package',
-    version: "0.1",
+    version: "0.0.1",
     name: "cglace:kurento-client",
     git: "https://github.com/cglace/meteor-kurento-client"
 });
 
 Npm.depends({
-   'kurento-client': '6.0.2'
+   'kurento-client': '6.0.1'
 });
 
 Package.onUse(function(api) {
-    api.addFiles
+    api.addFiles([
+       'lib/kurento-client.js'
+    ], ['server']);
+
+    api.export(['kurentoClient']);
 });
