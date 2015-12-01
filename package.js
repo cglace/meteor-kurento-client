6a666227c7f5dc7407d223d529d88e36b6ff155e
cglace <cglace@silverpop.com>
2015-12-01 10:25:06 -0500

    bump kurento client version

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     summary: 'Media API for the Kurento Web SDK via the NPM kurento-client-js package',
-    version: "0.0.5",
+    version: "0.0.6",
     name: "cglace:kurento-client",
     git: "https://github.com/cglace/meteor-kurento-client"
 });
 
 Npm.depends({
-   'kurento-client': '6.0.1'
+   'kurento-client': '6.2.0'
 });
 
 Package.onUse(function(api) {
